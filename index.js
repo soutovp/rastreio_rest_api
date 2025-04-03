b72c6567076e599a731311b3ba32c45804828673
Fernando Souto <fernandoandradesouto@hotmail.com>
2025-04-03 09:08:04 -0300

    merge

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-import express from 'express';
+import express, { json } from 'express';
+import env from 'dotenv';
+env.config();
 const app = express();
+
+app.listen(process.env.PORT, () => {
+	console.log(`Servidor rodando na porta ${process.env.PORT}`);
+});
