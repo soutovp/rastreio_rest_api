3bc135d0eb4c1f3ceb7c07e11e82e2c0c67e69c9
Fernando Souto <fernandoandradesouto@hotmail.com>
2025-04-09 16:58:13 -0300

    update

--- index.js
+++ index.js
@@ -17,35 +17,31 @@ app.get('/', async (req, res) => {
 	console.log(`GET		| Token é: ${token}`);
 	res.json({ token: token });
 });
-// app.post('/pedidos', async (req, res) => {
-// 	const bodyData = req.body;
-// 	if (typeof bodyData === 'undefined') res.send(JSON.stringify({ status: 403, message: 'Body is missing on the request.\n undefined' }));
-// 	// res.json(bodyData);
-// 	const validationResponse = await validation.validate();
-// 	console.log('PEDIDOS | Token é: ', validationResponse.token);
-// 	if (validationResponse.error) response.json({ error: validationResponse.error });
-// 	await fetch('https://www.nortondistribuidora.com.br/ws/v1/order/list', {
-// 		method: 'POST',
-// 		mode: 'cors',
-// 		headers: {
-// 			'Content-Type': 'application/json',
-// 			'access-token': validationResponse.token,
-// 		},
-// 		body: JSON.stringify(bodyData),
-// 	})
-// 		.then((resp) => {
-// 			console.log('--- RESPONSE ---');
-// 			console.log(response);
-// 			return resp.json();
-// 		})
-// 		.then((data) => {
-// 			console.log('--- DATA ---');
-// 			console.log(data);
-// 			if (data === undefined) return res.json({ message: 'No data...' });
-// 			if (data.status === 403 && data.reason === 'Forbidden') return { status: 403, message: data.message };
-// 			res.json(JSON.stringify({ status: 200, data: data.orders }));
-// 		});
-// });
+app.post('/pedidos', async (req, res) => {
+	const bodyData = req.body;
+	if (typeof bodyData === 'undefined') res.send(JSON.stringify({ status: 403, message: 'Body is missing on the request.\n undefined' }));
+	const validationResponse = await validation.authenticate();
+	if (validationResponse.error) response.json({ error: validationResponse.error });
+	await fetch('https://www.nortondistribuidora.com.br/ws/v1/order/list', {
+		method: 'POST',
+		mode: 'cors',
+		headers: {
+			'Content-Type': 'application/json',
+			'access-token': validationResponse,
+		},
+		body: JSON.stringify(bodyData),
+	})
+		.then((resp) => {
+			return resp.json();
+		})
+		.then((data) => {
+			console.log('PEDIDOS	| DATA :');
+			console.log(data);
+			if (data === undefined) return res.json({ message: 'No data...' });
+			if (data.status === 403 && data.reason === 'Forbidden') return { status: 403, message: data.message };
+			res.json(JSON.stringify({ status: 200, data: data.orders }));
+		});
+});
 
 app.listen(process.env.PORT, () => {
 	console.log(`Servidor rodando na porta ${process.env.PORT}`);
--- src/services/validation/validation.js
+++ src/services/validation/validation.js
@@ -75,8 +75,8 @@ export default class Validation {
 						this.saveDataDB(data);
 						return data.token;
 					}
-					if (data.status === 403) return this[_token];
-					return data;
+					if (data.status === 403) return this.token;
+					return this.token;
 				})
 				.catch((error) => error);
 		} else {
@@ -85,95 +85,3 @@ export default class Validation {
 		}
 	};
 }
-
-// export default function Validation(key) {
-// 	/**
-// 	 * Crie uma validação sempre que for acessar alguma rota.
-// 	 * É necessário se passar uma chave de acesso da iSet para acessar as rotas que precisam ser validadas.
-// 	 */
-// 	const _key = `Basic ${btoa(`${process.env.ISET_ACCESS_USER}:${process.env.ISET_ACCESS_KEY}`)}`;
-// 	const salvarDadosMemoria = function () {
-// 		fs.readFile(
-// 			'./src/db/token.json',
-// 			'utf-8',
-// 			function (err, data) {
-// 				if (err) console.log(`Erro: ${err}`);
-// 				if (data) {
-// 					const parsedData = JSON.parse(data);
-// 					const token = parsedData.iSet.token;
-// 					const expires_in = parsedData.iSet.expires_in;
-
-// 					if (token) this.token = token;
-// 					if (expires_in) this.expires_in = expires_in;
-// 				} else {
-// 					console.log('Não há dados salvos de acesso.\nPor favor solicite um novo Token de acesso!');
-// 					return;
-// 				}
-// 			}.bind(this)
-// 		);
-// 	}.bind(this);
-
-// 	const salvarDadosBD = function (objetoDados) {
-// 		if (typeof objetoDados !== 'string') {
-// 			console.log('Salvando no BD');
-// 			// console.log(JSON.stringify(objetoDados));
-// 			fs.writeFile('./src/db/token.json', JSON.stringify(objetoDados), (err) => {
-// 				console.log(err);
-// 			});
-// 		} else {
-// 			console.log('Salvando no BD');
-// 			// console.log(objetoDados);
-// 			fs.writeFile('./src/db/token.json', objetoDados, (err) => {
-// 				console.log(err);
-// 			});
-// 		}
-// 	};
-// 	this.token = '';
-// 	this.expires_in = '';
-// 	this.getDate = function () {
-// 		return parseInt(new Date().getTime() / 1000);
-// 	};
-// 	this.validate = async function () {
-// 		return new Promise(async (resolve, reject) => {
-// 			console.log('VALIDATE | Acessando validação ==============');
-// 			console.log('VALIDATE |', this.expires_in, ` é menor que?`);
-// 			console.log('VALIDATE |', this.getDate());
-// 			if (this.expires_in < this.getDate()) {
-// 				console.log('VALIDATE | Será emitido um novo token...');
-// 				fetch('https://www.nortondistribuidora.com.br/ws/v1/oauth', {
-// 					method: 'POST',
-// 					headers: {
-// 						Authorization: _key,
-// 					},
-// 				})
-// 					.then((res) => {
-// 						console.log(res);
-// 						return res.json();
-// 					})
-// 					.then((data) => {
-// 						if (data.status !== 403) {
-// 							let _token = data.token ? data.token : 'N/A';
-// 							let _expires_in = data.expires_in ? data.expires_in : 'N/A';
-// 							const newData = {
-// 								iSet: {
-// 									token: _token,
-// 									expires_in: _expires_in,
-// 								},
-// 							};
-// 							salvarDadosBD(newData);
-// 							const inf = JSON.stringify(newData.iSet.token);
-// 							console.log('VALIDATE | Token finalizado ============\n', inf);
-// 						}
-// 						return resolve({ token: newData.iSet.token, expires_in: newData.iSet.expires_in });
-// 					})
-// 					.catch((err) => {
-// 						return reject({ error: err });
-// 					});
-// 			} else {
-// 				console.log('VALIDATE | O token ainda é válido...');
-// 				return resolve({ token: this.token, expires_in: this.expires_in });
-// 			}
-// 		});
-// 	};
-// 	salvarDadosMemoria();
-// }
